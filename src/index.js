ea67e9261c8fbdfb1a90242632427b002c623f37
deeps <deepsehaj249@gmail.com>
2024-05-29 16:49:33 +0530

    multer

--- src/index.js
+++ src/index.js
@@ -7,8 +7,20 @@ dotenv.config({
     path : './env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+  app.on("error",(error)=>{
+    console.log("ERROR:",error);
+    throw error;
+   })
 
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`server is running at port :${process.env.PORT}`)
+  })
+})
+.catch((err)=>{
+  console.log("Mongo db connection failed",err);
+})
 
 
 
